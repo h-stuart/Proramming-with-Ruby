5112c0307cf3212ba0f6a275e906036fe9d1ebe5
Harold Stuart <harold.stuart@tripleseat.com>
2023-02-16 16:04:50 -0500

    Exercise 3

---
+++ basics/exercise3.rb
@@ -0,0 +1,14 @@
+
+blockbuster_movies = { 
+:jaws => 1975, 
+:shrek2 => 2004, 
+:ironman3 => 2013, 
+:shrek => 2001, 
+:superman2 => 1981, 
+}
+
+puts blockbuster_movies[:jaws]
+puts blockbuster_movies[:shrek2]
+puts blockbuster_movies[:ironman3]
+puts blockbuster_movies[:shrek]
+puts blockbuster_movies[:superman2]

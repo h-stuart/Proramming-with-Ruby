3a9a7b2fc664f9b441b37dcad4944902f09d6252
Harold Stuart <harold.stuart@tripleseat.com>
2023-02-22 00:25:11 -0500

    Print Until Exercise

---
+++ Exercises/until.rb
@@ -0,0 +1,13 @@
+numbers = [7, 9, 13, 25, 18]
+
+until numbers.size == 0
+  puts numbers.shift
+end
+
+# Another way to solve this using .empty?
+# numbers = [7, 9, 13, 25, 18]
+
+# until numbers.empty?
+#  puts numbers.shift
+# end
+
